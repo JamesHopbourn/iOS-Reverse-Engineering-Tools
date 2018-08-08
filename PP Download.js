d69b5728493f954f12fe9736829b752b9a336f66
James Hopbourn <james@mac.local>
2018-08-08 17:19:38 +0800

    JS 代码修改为当前窗口打开
    Signed-off-by: James Hopbourn <james@mac.local>

--- PP Download.js
+++ PP Download.js
@@ -2,5 +2,5 @@ javascript:(function(){
 	html = document.body.innerHTML;
 	url = html.match(/appdownurl=".*?"/);
 	url = url.toString().replace(/\"|appdownurl=/g, "");
-	window.open(atob(url));
+	window.open(atob(url),'_self');
 })()
